45916a4b5df0167fb83a98f47a692771dcd63ac2
Germano <bgermano89@gmail.com>
2018-08-26 20:27:58 -0300

    event-biding

--- data-biding/src/app/data-biding/data-biding.component.ts
+++ data-biding/src/app/data-biding/data-biding.component.ts
@@ -9,7 +9,10 @@ export class DataBidingComponent implements OnInit {
 
   url = 'http://loiane.com';
   cursoAngular =  true;
-  urlImagem = 'http://lorempixel.com/400/200/nature/';
+  urlImagem: string = 'http://lorempixel.com/400/200/nature/';
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
 
   getCurtirCurso() {
     return true;
@@ -18,6 +21,24 @@ export class DataBidingComponent implements OnInit {
   getValor() {
     return 1;
   }
+
+  botaoClicado(){
+    alert('botao clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    //console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOver(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
   constructor() { }
 
   ngOnInit() {
